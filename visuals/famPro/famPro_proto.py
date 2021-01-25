feac17ba2c3e25799df13abb1ce4e898c0cf8a65
ilenzio <westfury@gmail.com>
2021-01-25 18:20:42 -0500

    Cleaned up app file famPro_proto

--- visuals/famPro/famPro_proto.py
+++ visuals/famPro/famPro_proto.py
@@ -8,7 +8,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import df_cleaner
 
-# st.title("FAMPro")
+from datetime import datetime
+
 """ # FAMPro
     ##### F`amily Promise`   A`nalytics`   M`ode`    Pro`totype`
 """
@@ -18,22 +19,6 @@ file = "https://raw.githubusercontent.com/Lambda-School-Labs/family-promise-spok
 df = pd.read_csv(file, parse_dates=["Enroll Date", "Exit Date"])
 
 
-
-# cleaner = df_cleaner.Cleaner()
-# renamed_df = cleaner.renameColumns(df)
-# new_df = cleaner.set_dtypes(renamed_df)
-# cleaned_df = cleaner.recategorization(new_df)
-
-# st.write(df.head())
-
-# Seaborn viz
-
-
-# fig = sns.displot(df, x="Current Age",  hue="Gender", bins=20, multiple = "stack")
-
-# st.pyplot(fig)
-
-from datetime import datetime
 
 minDate = list(df["Enroll Date"].sort_values(axis=0, ascending=True).head())[0].to_pydatetime()
 maxDate = list(df["Enroll Date"].sort_values(axis=0, ascending=True).tail())[-1].to_pydatetime()
@@ -56,11 +41,6 @@ st.write("Analyzing Range: ", start_time, "to ", end_time)
 
 
 
-
-
-
-
-
 # altair viz
 import altair as alt
 
@@ -70,27 +50,13 @@ con_noExit = df["Exit Date"].isnull() == True
 
 con_dtRange = (df["Enroll Date"] >= start_time) & (df["Enroll Date"] <= end_time)
 currEnrolled_df = df[con_dtRange]
-# con_dtGreater2017 = cleaned_df["Enroll Date"] > pd.Timestamp(2019,12, 31)
-# cleaned_df[con_dtGreater2017].shape
 
-
-
-# Non-Interactive chart
-# test = alt.Chart(source).mark_bar().encode(
-#     alt.Y("Current Age", title = "Current Guest Age"),
-#     alt.X('count()'),
-#     color="Gender",
-   
-# )
 
 # 3 different dfs
 # total pop
 # head of Households
 # dependents
 
-# Currently enrolled 
-# con_noExit = df["Exit Date"].isnull() == True
-# currEnrolled_df = df[con_noExit]
 
 # head of Households from currEnrolled
 con_Hoh = currEnrolled_df["Relationship to HoH"] == "Self" #or cleaned_df["Relationship to HoH"] == "Significant Other (Non-Married)"
@@ -110,17 +76,12 @@ featureOption = st.sidebar.selectbox("Feature ",
 
 "Viewing: ", featureOption
 
-# st.altair_chart(test)
-
-# source =currEnrolled_df
-
 source = enrolledDataList[featureOption]
 
-
-# Interactive test
+# Interactive Bar chart (Histogram)  of Population during Enrollment Range
 selection = alt.selection_multi(fields=["Gender"], bind="legend")
 
-test2 = alt.Chart(source).mark_bar().encode(
+populationBarChart = alt.Chart(source).mark_bar().encode(
     alt.X("Current Age", title = "Current Guest Age"),
     alt.Y('count()', stack = None),
     color="Gender",
@@ -129,5 +90,5 @@ test2 = alt.Chart(source).mark_bar().encode(
     selection
 ).properties(width=550).interactive()
 
-st.altair_chart(test2)
+st.altair_chart(populationBarChart)
 
