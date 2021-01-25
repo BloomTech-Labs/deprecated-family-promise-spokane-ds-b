85d23e72acb6ca983ce8647835aff77d1ad4397e
ilenzio <westfury@gmail.com>
2021-01-24 23:54:52 -0500

    Filter by Enrollment date and Relationship to Hoh

--- visuals/famPro/famPro_proto.py
+++ visuals/famPro/famPro_proto.py
@@ -8,12 +8,15 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import df_cleaner
 
-st.title("FAMPro")
+# st.title("FAMPro")
+""" # FAMPro
+    ##### F`amily Promise`   A`nalytics`   M`ode`    Pro`totype`
+"""
 
 
 # file = "https://raw.githubusercontent.com/Lambda-School-Labs/family-promise-spokane-ds-a/main/All_data_with_exits.csv"
 file = "/Users/kellycho/Desktop/Repos/family-promise-spokane-ds-b/visuals/famPro/cleanedData02.csv"
-df = pd.read_csv(file)
+df = pd.read_csv(file, parse_dates=["Enroll Date", "Exit Date"])
 
 
 
@@ -22,41 +25,40 @@ df = pd.read_csv(file)
 # new_df = cleaner.set_dtypes(renamed_df)
 # cleaned_df = cleaner.recategorization(new_df)
 
-
-# Seaborn viz
 # st.write(df.head())
 
+# Seaborn viz
 
-fig = sns.displot(df, x="Current Age",  hue="Gender", bins=20, multiple = "stack")
 
-st.pyplot(fig)
+# fig = sns.displot(df, x="Current Age",  hue="Gender", bins=20, multiple = "stack")
 
-# 3 different dfs
-# total pop
-# head of Households
-# dependents
+# st.pyplot(fig)
 
-# Currently enrolled 
-con_noExit = df["Exit Date"].isnull() == True
-currEnrolled_df = df[con_noExit]
+from datetime import datetime
 
-# head of Households from currEnrolled
-con_Hoh = currEnrolled_df["Relationship to HoH"] == "Self" #or cleaned_df["Relationship to HoH"] == "Significant Other (Non-Married)"
-hoh_cleaned_df = currEnrolled_df[con_Hoh]
+minDate = list(df["Enroll Date"].sort_values(axis=0, ascending=True).head())[0].to_pydatetime()
+maxDate = list(df["Enroll Date"].sort_values(axis=0, ascending=True).tail())[-1].to_pydatetime()
+"""
 
-# dependents from currEnrolled
-con_dep = currEnrolled_df["Relationship to HoH"] != "Self"
-dep_cleaned_df = currEnrolled_df[con_dep]
 
-enrolledDataList = {"All Currently Enrolled": currEnrolled_df, 
-                    "Heads of Household Enrolled": hoh_cleaned_df,
-                    "Dependents Enrolled": dep_cleaned_df
-}
+"""
+start_time = st.sidebar.slider(label = "Date Range Start",
+            min_value=minDate,
+            max_value=maxDate,
+            format="MM/DD/YY")
 
-featureOption = st.sidebar.selectbox("Feature ", 
-    list(enrolledDataList.keys()))
+end_time = st.sidebar.slider(label = "Date Range End",
+            min_value=minDate,
+            max_value=maxDate,
+            format="MM/DD/YY")
 
-"Viewing: ", featureOption
+
+st.write("Analyzing Range: ", start_time, "to ", end_time)
+
+
+
+
+
 
 
 
@@ -65,9 +67,14 @@ import altair as alt
 
 con_noExit = df["Exit Date"].isnull() == True
 # noExit_cleaned_df = df[con_noExit]
-currEnrolled_df = df[con_noExit]
+# currEnrolled_df = df[con_noExit]
 
-source =currEnrolled_df
+con_dtRange = (df["Enroll Date"] >= start_time) & (df["Enroll Date"] <= end_time)
+currEnrolled_df = df[con_dtRange]
+# con_dtGreater2017 = cleaned_df["Enroll Date"] > pd.Timestamp(2019,12, 31)
+# cleaned_df[con_dtGreater2017].shape
+
+
 
 # Non-Interactive chart
 # test = alt.Chart(source).mark_bar().encode(
@@ -77,7 +84,39 @@ source =currEnrolled_df
    
 # )
 
+# 3 different dfs
+# total pop
+# head of Households
+# dependents
+
+# Currently enrolled 
+# con_noExit = df["Exit Date"].isnull() == True
+# currEnrolled_df = df[con_noExit]
+
+# head of Households from currEnrolled
+con_Hoh = currEnrolled_df["Relationship to HoH"] == "Self" #or cleaned_df["Relationship to HoH"] == "Significant Other (Non-Married)"
+hoh_cleaned_df = currEnrolled_df[con_Hoh]
+
+# dependents from currEnrolled
+con_dep = currEnrolled_df["Relationship to HoH"] != "Self"
+dep_cleaned_df = currEnrolled_df[con_dep]
+
+enrolledDataList = {"All Guests Enrolled": currEnrolled_df, 
+                    "Heads of Household Enrolled": hoh_cleaned_df,
+                    "Dependents Enrolled": dep_cleaned_df
+}
+
+featureOption = st.sidebar.selectbox("Feature ", 
+    list(enrolledDataList.keys()))
+
+"Viewing: ", featureOption
+
 # st.altair_chart(test)
+
+# source =currEnrolled_df
+
+source = enrolledDataList[featureOption]
+
 
 # Interactive test
 selection = alt.selection_multi(fields=["Gender"], bind="legend")
